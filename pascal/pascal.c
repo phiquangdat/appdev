bb6fe80f09f869ee33ecd198ab1222d50cf71023
phiquangdat <datphi020603@gmail.com>
2025-11-13 10:42:20 +0200

    Implementation for Trinomial

--- pascal/pascal.c
+++ pascal/pascal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "screen.h"
 // function definition
 int factorial(int n) {
 	if (n == 0) return 1; 
@@ -10,15 +11,20 @@ int n_choose_k(int n, int k) {
 }
 
 void pascal_by_shoulder_add(int a[], int row, int current_row){
-	for(int i = 0; i <= current_row; i++)
-		printf("%5d", a[i]);
-	printf("\n");
-	
 	if(row == current_row) return; //we are at the last row
-	int b[100] = {1}; // new row
-	for(int i = 1; i <= current_row; i++){ //do shoulder add
-		b[i] = a[i-1] + a[i];
+
+	gotoXY(40 + (row - current_row) * 5, 10 + current_row * 2);
+	for(int i = 0; i <= current_row * 2; i++)
+	{
+		printf("%5d", a[i]);
 	}
-	b[current_row + 1] = 1; //the last element in the new row
+	printf("\n");
+
+	int b[100] = {1}; // new row
+	b[1] = a[0] + a[1];
+	for(int i = 2; i <= (current_row + 1) * 2; i++){ //do shoulder add
+		b[i] = a[i - 2]  + a[i - 1] + a[i];
+	}
+	b[current_row * 2 + 2] = 1; //the last element in the new row
 	pascal_by_shoulder_add(b, row, current_row + 1);
 }
