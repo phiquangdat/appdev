48f72d3432d281d31e870a026453019e471058e2
phiquangdat <datphi020603@gmail.com>
2025-11-28 13:05:36 +0200

    Hexagon matrix

---
+++ hexagon_matrix/main.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+#include "pascal.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "screen.h"
+
+int main(void) {
+	int row;
+	printf("Pascal triangle, enter the number of rows: ");
+	scanf("%d", &row);
+	int a[100] = {1};
+	pascal_by_shoulder_add(a, row, row);
+/*	setBGcolor(BLACK);
+	clearScreen();
+	srand(time(NULL));
+	for(int i = 0; i < row; i++) {
+		gotoXY(40 + (row - i) * 2, 10 + i);
+		for(int j = 0; j <= i; j++) {
+			setFGcolor(RED + rand() % 7);
+			printf("%4d", n_choose_k(i, j));
+		}
+		printf("\n");
+	}*/
+	printf("\x1b[0m");
+}
