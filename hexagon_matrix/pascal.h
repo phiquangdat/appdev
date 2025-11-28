dde43c61907987a2f7aa56df7016ad73bcb6e5e8
phiquangdat <datphi020603@gmail.com>
2025-11-28 13:05:36 +0200

    Hexagon matrix

---
+++ hexagon_matrix/pascal.h
@@ -0,0 +1,8 @@
+#pragma once
+// there are usually 3 parts in a header file:
+// constant declarations, struct/class definitions, function declarations
+
+// function declarations
+int factorial(int n);
+int n_choose_k(int n, int k);
+void pascal_by_shoulder_add(int a[], int row, int current_row);
