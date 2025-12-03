206c32672a26363a77061526eaf878c450b5ac79
Dat Phi <88042242+phiquangdat@users.noreply.github.com>
2025-12-03 23:22:58 +0200

    Remove unnecessary things

--- pascal/pascal.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// there are usually 3 parts in a header file:
-// constant declarations, struct/class definitions, function declarations
-
-// function declarations
-int factorial(int n);
-int n_choose_k(int n, int k);
-void pascal_by_shoulder_add(int a[], int row, int current_row);
