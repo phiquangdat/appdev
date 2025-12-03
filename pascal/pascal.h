1ac2470773a159f8b3f48f388d91f72ede67c1d8
Dat Phi <88042242+phiquangdat@users.noreply.github.com>
2025-12-04 00:42:54 +0200

    Clean up symmetrical project

--- pascal/pascal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// there are usually 3 parts in a header file:
-// constant declarations, struct/class definitions, function declarations
-
-// function declarations
-int factorial(int n);
-int n_choose_k(int n, int k);
