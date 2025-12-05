104d76dda05d5ebdfe818ff454de1d61122b4efd
phiquangdat <datphi020603@gmail.com>
2025-12-05 16:49:08 +0200

    Refactored spiral_matrix project to use a stack-allocated fixed array * Corrected I/O stream handling prompts now print to stderr, allowing matrix output to be redirected to a file via stdout * Put that redirected output command in Makefile.

--- spiral_matrix/spiral_matrix.h
+++ spiral_matrix/spiral_matrix.h
@@ -3,5 +3,8 @@
 // there are usually 3 parts in a header file:
 // constant declarations, struct/class definitions, function declarations
 
+// constant declarations
+#define MAX_SIZE 100
+
 // function declarations
-void spiral_matrix(int row, int** numbers, FILE* fp);
+void spiral_matrix(int row, int numbers[MAX_SIZE][MAX_SIZE]);
